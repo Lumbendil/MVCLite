7fc52dad53625edd91c69f391dbd88f58bd1dab5
Roger Llopart Pla <lumbendil@gmail.com>
2011-02-06 22:22:08 +0100

    RouterTest tests expected behaviour (missing 1 case).

--- tests/class/routerTest.php
+++ tests/class/routerTest.php
@@ -16,11 +16,33 @@ class RouterTest extends PHPUnit_Framework_TestCase
 		vfsStream::setUp( 'root' );
         vfsStream::newFile( 'config.ini' )->at( vfsStreamWrapper::getRoot() );
 
-		$this->router = new Router( vfsStream::url( 'root/config.ini' ) );
+        $config_file = vfsStream::url( 'root/config.ini' );
+
+        $config_content = <<<CONFIG
+['%^/random-uri-(with)-(params)$%']
+controller = 'ControllerToBeUsed'
+action = 'actionToBeRun'
+CONFIG;
+		file_put_contents( $config_file , $config_content );
+
+		$this->router = new Router( $config_file );
 	}
 
-	public function testGetControler()
+	public function testParseUriWithWrongUri()
 	{
-		$this->assertEquals('', $this->router->getController() );
+		$this->markIncompleteTest();
+	}
+
+	public function testParseUri()
+	{
+		$this->assertEquals( '', $this->router->getController(), 'Controller name starts empty' );
+		$this->assertEquals( '', $this->router->getAction(), 'Action starts empty' );
+		$this->assertEquals( array(), $this->router->getParams(), 'Params starts empty' );
+
+		$this->router->parseUri( '/random-uri-with-params' );
+
+		$this->assertEquals( 'ControllerToBeUsed', $this->router->getController(), 'Controller name is modified.' );
+		$this->assertEquals( 'actionToBeRun', $this->router->getAction(), 'Action is modified' );
+		$this->assertEquals( array( 'with', 'params' ), $this->router->getParams(), 'Params are modified' );
 	}
 }
