0e17ef7c11de12f2a0818a3db07a5c57698ddcba
Roger Llopart Pla <lumbendil@gmail.com>
2011-02-06 22:53:57 +0100

    Fixed test bootstrap.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,3 @@ require_once ROOT_PATH . 'app/config/defines.php';
 require_once CORE_PATH . 'autoload.php';
 
 Autoloader::startInstance();
-
-PHPUnit_Util_Filter::addDirectoryToFilter(
-  ROOT_PATH . 'app', '.php'
-);
