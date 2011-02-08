0a2e73fcef65b3737bf9099cd68162a4a3ef31aa
Roger Llopart Pla <lumbendil@gmail.com>
2011-02-08 14:09:58 +0100

    Added a function to bootstrap to make Factory mocking easier.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,3 +4,19 @@ require_once ROOT_PATH . 'app/config/defines.php';
 require_once CORE_PATH . 'autoload.php';
 
 Autoloader::startInstance();
+
+function factoryMocker( $factory_name, $factory_method, $factory_returns )
+{
+	$code = <<<FACTORYCODE
+class $factory_name
+{
+	public static \$returns = NULL;
+	public static function $factory_method ( \$class )
+	{
+		return self::\$returns[\$class];
+	}
+}
+FACTORYCODE;
+	eval( $code );
+	$factory_name::$returns = $factory_returns;
+}
